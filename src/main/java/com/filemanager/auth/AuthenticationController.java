dc545d0c0d6c0f70a62941c1ad7d38f8d504dc6a
Bratucu Bogdan <bratucubogdan@gmail.com>
2023-04-04 10:56:19 +0200

    Login form and registration Form

--- src/main/java/com/filemanager/auth/AuthenticationController.java
+++ src/main/java/com/filemanager/auth/AuthenticationController.java
@@ -1,11 +1,16 @@
 package com.filemanager.auth;
 
+import com.filemanager.file.FileModel;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -14,17 +19,51 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
-    @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
-    ) {
-        return ResponseEntity.ok(service.register(request));
+    public String getToken() {
+        return token;
     }
+
+    private String token;
+
+    @ModelAttribute
+    public void setRequestHeader(HttpServletResponse response) {
+        response.setHeader("Authorization", "Bearer " + token);
+    }
+
+    @GetMapping("/registerForm")
+    public ModelAndView showRegisterForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("registrationForm");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public void register(
+            @ModelAttribute("registerRequest") RegisterRequest registerRequest,
+            HttpServletResponse response
+    ) throws IOException {
+        service.register(registerRequest);
+        response.sendRedirect("/api/v1/auth/login");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        modelAndView.addObject("authRequest", new AuthenticationRequest());
+        return modelAndView;
+    }
+
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
-    ) {
-        return ResponseEntity.ok(service.authenticate(request));
+    public void authenticate(
+            @ModelAttribute("authRequest") AuthenticationRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        token = service.authenticate(request).getToken();
+        System.out.println(token);
+
+        response.sendRedirect("/search");
     }
 
 
